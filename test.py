c27f24fef7c0757bdb9bd4c1d1ac718970d89282
mortada gzar <mortadagzar@gmail.com>
2018-11-13 13:43:52 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,7 @@
-
-class human():
-      def arm(tutu):
-          print('this is an arm')
-      def head(self):
-          print ('this is a head')   
-
-human().arm()  
-human().head()   
-print(type(__doc__))
-print(type('guguh'))  
+class mina():
+    '''tguis id mou oh mou '''
+    def name(self):
+        print('my name is miran not mohamed')
+print(mina() )  
 
 
-       
