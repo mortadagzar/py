d70f1e25276a270e32d9d5a69a4621c1b83c2f41
mortada gzar <mortadagzar@gmail.com>
2018-11-13 23:14:49 -0800

    Update py.py

--- py.py
+++ py.py
@@ -1,19 +1,3 @@
 Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
 Type "copyright", "credits" or "license()" for more information.
->>> rest="mohammed"
->>> rest.startswith("my")
-False
->>> rest.find("m")
-0
->>> 
-hello
-Traceback (most recent call last):
-  File "<pyshell#3>", line 2, in <module>
-    hello
-NameError: name 'hello' is not defined
->>> 
-ret="uhuuh"
->>> 
-print(ret)
-uhuuh
 >>> 
