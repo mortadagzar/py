2b400f3da0dab28b8c563664d543a5af1c33fdd4
mortada gzar <mortadagzar@gmail.com>
2018-11-16 15:54:55 -0800

    Update python.py

--- python.py
+++ python.py
@@ -1,18 +1,3 @@
-print("#"*90)
-print("Heloo")
-print("#"*90)
-def add(x,y):
-    print ("the total is :",x+y)
-
-def sub(x,y):
-    print ("the extraois :",x-y)
-def mult(x,y):
-     print ("the multiple is :",x*y)
-
-x=int(input("input the value of X: "))
-y=int(input("input the value of Y:"))
-add(x,y)
-sub(x,y)
-mult(x,y)
-
-
+import math
+x=math.cos(78)
+print(x)
