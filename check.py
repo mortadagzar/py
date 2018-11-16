1af4eefb2f938a5b8d4bb7a6bd3ec48716bbceaf
mortada gzar <mortadagzar@gmail.com>
2018-11-16 09:34:21 -0800

    Update check.py

---
+++ check.py
@@ -0,0 +1,10 @@
+email="mortada@gmail.com"
+password="q1w2e3"
+emailu=input("Enter your email: ")
+passwordu=input("Enter your password : ")
+if email==emailu and password==passwordu:
+    print("welcome! It's your account")
+else :
+    print("Incorrect password or e,ail")
+    
+        
