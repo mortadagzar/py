930709bfee1b9fa739b715f1e0a44b7072150f2f
mortada gzar <mortadagzar@gmail.com>
2018-11-16 11:19:24 -0800

    Update python.py

--- python.py
+++ python.py
@@ -1,9 +1,18 @@
-elsafy=input("Enter a value : ")
+print("#"*90)
+print("Heloo")
+print("#"*90)
+def add(x,y):
+    print ("the total is :",x+y)
 
-def myprint(elsafy):
-    
-    return elsafy
+def sub(x,y):
+    print ("the extraois :",x-y)
+def mult(x,y):
+     print ("the multiple is :",x*y)
+
+x=int(input("input the value of X: "))
+y=int(input("input the value of Y:"))
+add(x,y)
+sub(x,y)
+mult(x,y)
 
 
-x=myprint(elsafy)
-print(x)
