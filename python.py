c802f253ded2dd929157d201b240e607ac0c9833
mortada gzar <mortadagzar@gmail.com>
2018-11-16 10:44:37 -0800

    Update python.py

--- python.py
+++ python.py
@@ -1,13 +1,9 @@
-usernum=input("Enter a number : ")
-papo=[1,5,87,54,654]
-for number in papo:
-    
-    if number==usernum:
-        print("it is here!")
-    else:
-        usernum=input("Enter a number : ")
+elsafy=input("Enter a value : ")
 
-        
-        
-        
+def myprint(elsafy):
     
+    return elsafy
+
+
+x=myprint(elsafy)
+print(x)
