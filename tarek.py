55f10007c7cc85dd7e35ea85feb91ccb54a6358d
mortada gzar <mortadagzar@gmail.com>
2018-11-16 16:50:40 -0800

    j

---
+++ tarek.py
@@ -0,0 +1,13 @@
+'''
+this module has 3 functions each function prints kind of line
+'''
+
+
+def myname():
+    print ("mortada gzar")
+def myage():
+    print ("36")
+def sex():
+    print("male")
+
+    
