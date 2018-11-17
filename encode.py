0089a68a39d9e2d5c9ab1e7266871be3c88c3324
mortada gzar <mortadagzar@gmail.com>
2018-11-17 14:34:41 -0800

    Update encode.py

--- encode.py
+++ encode.py
@@ -1,8 +1,3 @@
-from hashlib import *
-file=input("enter the file name : ")
-with open(file,mode="r") as f:
-    
-    for line in f:
-        line=line.strip()
-        hashfile=md5(line.encode()).hexdigest()
-        print(hashfile)
+x="jjiijijij"
+class human( ):
+    print(x)
